ebe549e283180cf68aa1ab3ba512c309660bdefe
Fritz Grimpen <fritz@grimpen.net>
2012-11-22 18:17:03 +0000

    More serious

--- popcorn/logs.py
+++ popcorn/logs.py
@@ -5,6 +5,7 @@ import sys
 import time
 import calendar
 import popcorn
+import pymongo
 
 def normalize_author(author):
 	if author[0] in ['@', '+', '~']:
@@ -20,8 +21,15 @@ def main():
 
 	argparser = argparse.ArgumentParser(description="Read IRC logs into the database")
 	argparser.add_argument('-c', '--channel', default=None)
+	argparser.add_argument('-u', '--update', default=False, action='store_true')
 
 	args = argparser.parse_args()
+	if args.update:
+		last_entry = popcorn.db.messages.find({"channel": args.channel}).sort("time", pymongo.DESCENDING).limit(1)
+		if not last_entry.count():
+			last_entry = 0
+		else:
+			last_entry = last_entry[0]["time"]
 
 	for line in sys.stdin:
 		line = line.strip()
@@ -33,6 +41,9 @@ def main():
 			continue
 		time = parse_weechat_time(time)
 		author = normalize_author(author)
+
+		if args.update and time <= last_entry:
+			continue
 
 		message = {
 			"time": time,
